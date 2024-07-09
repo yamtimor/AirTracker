68dd1deff1b5511ce69f900d591b9c6fbf23ce60
yamtimor <yamtimor@gmail.com>
2024-07-09 21:49:15 +0300

    split the url

--- src/flight_data_service.py
+++ src/flight_data_service.py
@@ -1,15 +1,19 @@
 import requests
+from pprint import pprint
 
-API_BASE_URL = "https://opensky-network.org/api/states/all"
+API_BASE_URL = "https://opensky-network.org/"
+ROUTE = "api/states/all?icao24=3c6444&icao24=3e1bf9"
 
 def get_flight_data():
     try:
-        response = requests.get(API_BASE_URL)
+        response = requests.get(API_BASE_URL+ROUTE)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching flight data: {e}")
         return None
+
+
 
 if __name__ == "__main__":
     flight_data = get_flight_data()
